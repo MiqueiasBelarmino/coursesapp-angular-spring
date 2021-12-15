549a24a871bed640ec9a5b0310f0f36db078710a
Miquéias Belarmino <miqueiascostabac@gmail.com>
2021-12-15 08:39:10 -0300

    handling error  and MatDialog

--- coursesapp-angular/src/app/courses/courses.module.ts
+++ coursesapp-angular/src/app/courses/courses.module.ts
@@ -1,12 +1,14 @@
-import { AppMaterialModule } from './../shared/app-material/app-material.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+
+import { AppMaterialModule } from './../shared/app-material/app-material.module';
+import { SharedModule } from './../shared/shared.module';
+import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
+
 // import { MatCardModule } from '@angular/material/card';
 // import { MatTableModule } from '@angular/material/table';
 // import { MatToolbarModule } from '@angular/material/toolbar';
-
-import { CoursesRoutingModule } from './courses-routing.module';
-import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    AppMaterialModule
+    AppMaterialModule,
+    SharedModule
     // MatTableModule,
     // MatCardModule,
     // MatToolbarModule
--- coursesapp-angular/src/app/courses/courses/courses.component.ts
+++ coursesapp-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,7 @@
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
 
 import { Course } from './../model/course';
 import { CoursesService } from './../services/courses.service';
@@ -17,9 +19,20 @@ export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
   displayedColumns = ['name','category'];
 
-  constructor(private coursesService: CoursesService) {
+  constructor(private coursesService: CoursesService,public dialog: MatDialog) {
     /*this.courses = [];*/
-    this.courses$ = this.coursesService.findAll();
+    this.courses$ = this.coursesService.findAll().pipe(
+      catchError(error => {
+        this.onError('Fail to load courses.');
+        return of([]);
+      })
+    );
+  }
+
+  onError(message: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: message,
+    });
   }
 
   ngOnInit(): void {
