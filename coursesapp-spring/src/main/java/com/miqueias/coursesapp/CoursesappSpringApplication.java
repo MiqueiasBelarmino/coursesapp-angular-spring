095a9c4913720cf1efd16edff2583c2a18c17c50
Miquéias Belarmino <miqueiascostabac@gmail.com>
2021-12-17 10:59:07 -0300

    add spring

---
+++ coursesapp-spring/src/main/java/com/miqueias/coursesapp/CoursesappSpringApplication.java
@@ -0,0 +1,13 @@
+package com.miqueias.coursesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesappSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesappSpringApplication.class, args);
+	}
+
+}
